3a90535303453bfed189228750dc5f1b153336a1
Ivan Sapego <sapego-ivan@list.ru>
2024-03-06 13:36:30 +0300

    feat: added base auth logic

---
+++ src/pages/Login/styled.ts
@@ -0,0 +1,46 @@
+import { FlexMixin } from '@root/theme';
+import styled from 'styled-components';
+
+export const StyledLoginPage = styled.div`
+  ${FlexMixin({ direction: 'column' })}
+
+  width: 100%;
+  padding: 30px 0;
+  max-width: 450px;
+  margin: 0 auto;
+
+  & svg {
+    margin-bottom: 33px;
+  }
+`;
+
+export const LoginForm = styled.form`
+  width: 100%;
+`;
+
+export const SubmitButton = styled.input`
+  background: ${({ theme }) => theme.color.ltBlue};
+  border-radius: ${({ theme }) => theme.borderRadius.xl2}px;
+  font-size: ${({ theme }) => theme.fontSize.sm}px;
+  color: ${({ theme }) => theme.color.white};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  padding: 15px 0 19px 0;
+`;
+
+export const LoginTitle = styled.h3`
+  color: ${({ theme }) => theme.color.primary};
+  font-size: ${({ theme }) => theme.fontSize.xl3}px;
+  font-weight: ${({ theme }) => theme.fontWeight.black};
+  margin-bottom: 30px;
+`;
+
+export const FormField = styled.div`
+  margin-bottom: 24px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
