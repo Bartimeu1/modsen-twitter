c9343fe9418e13efec6c65fe86218b0dd3cfd546
Ivan Sapego <sapego-ivan@list.ru>
2024-03-16 18:37:50 +0300

    feat: added Error Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { ErrorBoundary } from '@components/ErrorBoundary';
 import { PrivateLayout } from '@components/PrivateLayout';
 import { PublicLayout } from '@components/PublicLayout';
 import { ToastList } from '@components/ToastList';
@@ -14,24 +15,26 @@ export const App = () => {
 
   return (
     <ThemeProvider theme={theme[currentTheme]}>
-      <HashRouter>
-        <Routes>
-          <Route path="*" element={<NotFoundPage />} />
-          <Route element={<PublicLayout />}>
-            {publicRoutes.map(({ id, path, element }) => (
-              <Route key={id} path={path} element={element} />
-            ))}
-          </Route>
-          <Route element={<PrivateLayout />}>
-            <Route path="/" element={<Navigate to="/profile" />} />
-            {privateRoutes.map(({ id, path, element }) => (
-              <Route key={id} path={path} element={element} />
-            ))}
-          </Route>
-        </Routes>
-      </HashRouter>
-      <ToastList />
-      <GlobalStyles />
+      <ErrorBoundary>
+        <HashRouter>
+          <Routes>
+            <Route path="*" element={<NotFoundPage />} />
+            <Route element={<PublicLayout />}>
+              {publicRoutes.map(({ id, path, element }) => (
+                <Route key={id} path={path} element={element} />
+              ))}
+            </Route>
+            <Route element={<PrivateLayout />}>
+              <Route path="/" element={<Navigate to="/profile" />} />
+              {privateRoutes.map(({ id, path, element }) => (
+                <Route key={id} path={path} element={element} />
+              ))}
+            </Route>
+          </Routes>
+        </HashRouter>
+        <ToastList />
+        <GlobalStyles />
+      </ErrorBoundary>
     </ThemeProvider>
   );
 };
