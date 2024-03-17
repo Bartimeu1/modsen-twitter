cfa67a3794611e136d3757801f5c6a88d7e768d5
Ivan Sapego <sapego-ivan@list.ru>
2024-03-17 14:23:21 +0300

    feat: added jest environment

---
+++ src/services/ConfigProvider/index.tsx
@@ -0,0 +1,22 @@
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+import { ErrorBoundary } from '@components/ErrorBoundary';
+import { GlobalStyles,theme } from '@root/theme';
+import { store } from '@store/store';
+import { ThemeProvider } from 'styled-components';
+
+import { IConfigProviderProps } from './types';
+
+export const ConfigProvider = ({ children }: IConfigProviderProps) => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme['dark']}>
+        <GlobalStyles />
+        <ErrorBoundary>
+          <HashRouter>{children}</HashRouter>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </Provider>
+  );
+};
